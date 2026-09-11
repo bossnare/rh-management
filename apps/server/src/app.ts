99203ce424128c835e61252e4a93bbfa8e3fbd1f
bossnare <razafimangagervaischristo@gmail.com>
2026-09-11 07:55:53 +0300

    ça ira bien

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -1,26 +1,19 @@
 import express from 'express';
-import connectDB from './config/db.js'; // mila an'io raha misaraka front-back
 import dotenv from 'dotenv';
 import cors from 'cors'; // mila an'io raha misaraka front-back
-import ping from './routes/ping.route.js';
 import verifyJWT from './middlewares/jwt.middleware.js';
 import morgan from 'morgan';
 import swaggerUi from 'swagger-ui-express';
 import swaggerJsDoc from 'swagger-jsdoc';
 import swaggerOptions from './config/swagger.js';
-import chicken from './routes/admin.route.js';
 
 dotenv.config();
 
 const app = express();
-// Setup Swagger documentation
-const specs = swaggerJsDoc(swaggerOptions);
-// Connect to MongoDB
-connectDB();
-// Middleware to parse JSON requests
-app.use(express.json());
-app.use(cors()); // 🔥 Mila an'io raha misaraka front-back
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(specs)); // Serve Swagger UI at /api-docs
+const specs = swaggerJsDoc(swaggerOptions); // Setup Swagger documentation
+app.use(express.json()); // Middleware to parse JSON requests
+app.use(cors());
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(specs)); // Serve Swagger UI at /docs
 app.use(morgan('dev'));
 
 // Routes
@@ -32,13 +25,11 @@ app.get('/', (req, res) => {
 });
 
 app.use(verifyJWT); // Middleware to verify JWT token
-app.use('/api', ping);
-app.use('/api/chicken', chicken);
 
 // Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running in https://localhost/${PORT}`);
 });
 // Export the app for testing purposes
 export default app;
